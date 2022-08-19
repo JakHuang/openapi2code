46b5292eaea44ff348f5555465b3b80f5820776a
JakHuang <352407406@qq.com>
2022-08-19 15:34:17 +0800

    递归折叠和展开节点

--- src/components/TreeSelector.vue
+++ src/components/TreeSelector.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="wrapper">
-    <el-tree :data="dataTree" node-key="id" show-checkbox ref="treeRef">
+    <el-tree
+      :data="dataTree"
+      :default-expanded-keys="[1]"
+      node-key="id"
+      show-checkbox
+      ref="treeRef"
+    >
       <template #default="{ node, data }">
         <div class="custom-tree-node">
           <key-value :field="data.key" :value="data.label" />
@@ -8,14 +14,14 @@
             <a
               v-if="!node.isLeaf && !node.expanded"
               class="btn-text"
-              @click.prevent.stop="() => expandedAll(node, data)"
+              @click.prevent.stop="() => recursivelySetExpanded(node, true)"
             >
               递归展开
             </a>
             <a
-              v-if="node.expanded"
+              v-if="!node.isLeaf && node.expanded"
               class="btn-text"
-              @click.prevent.stop="() => foldAll(node, data)"
+              @click.prevent.stop="() => recursivelySetExpanded(node, false)"
             >
               递归折叠
             </a>
@@ -29,49 +35,6 @@
 <script>
 import { isArray, isObject } from '../utils/type.js';
 import KeyValue from './KeyValue.vue';
-
-let nid = 1;
-
-const buildTreeData = (key, value, res) => {
-  if (isObject(value)) {
-    const res2 = [];
-    res.push({
-      id: nid++,
-      key,
-      label: value,
-      children: res2,
-    });
-    Object.entries(value).forEach(([key2, value2]) => {
-      buildTreeData(key2, value2, res2);
-    });
-  } else if (isArray(value)) {
-    const res2 = [];
-    res.push({
-      id: nid++,
-      key,
-      label: value,
-      children: res2,
-    });
-    value.forEach((item) => {
-      buildTreeData('', item, res2);
-    });
-  } else {
-    res.push({
-      id: nid++,
-      key,
-      label: value,
-    });
-  }
-};
-
-const recursivelySetExpanded = (node, val) => {
-  node.expanded = val;
-  if (Array.isArray(node.children)) {
-    node.children.forEach((item) => {
-      recursivelySetExpanded(item, val);
-    });
-  }
-};
 
 export default {
   props: {
@@ -95,18 +58,55 @@ export default {
         throw new Error('数据类型错误');
       }
       arr.forEach((item) => {
-        buildTreeData('', item, res);
+        this.buildTreeData('', item, res);
       });
       return res;
     },
   },
+  data() {
+    return {
+      nid: 1,
+    };
+  },
   methods: {
-    expandedAll(node, data) {
-      console.log(node);
-      recursivelySetExpanded(node, true);
+    buildTreeData(key, value, res) {
+      if (isObject(value)) {
+        const res2 = [];
+        res.push({
+          id: this.nid++,
+          key,
+          label: value,
+          children: res2,
+        });
+        Object.entries(value).forEach(([key2, value2]) => {
+          this.buildTreeData(key2, value2, res2);
+        });
+      } else if (isArray(value)) {
+        const res2 = [];
+        res.push({
+          id: this.nid++,
+          key,
+          label: value,
+          children: res2,
+        });
+        value.forEach((item) => {
+          this.buildTreeData('', item, res2);
+        });
+      } else {
+        res.push({
+          id: this.nid++,
+          key,
+          label: value,
+        });
+      }
     },
-    foldAll(node, data) {
-      recursivelySetExpanded(node, false);
+    recursivelySetExpanded(node, val) {
+      node.expanded = val;
+      if (Array.isArray(node.childNodes)) {
+        node.childNodes.forEach((item) => {
+          this.recursivelySetExpanded(item, val);
+        });
+      }
     },
   },
 };
